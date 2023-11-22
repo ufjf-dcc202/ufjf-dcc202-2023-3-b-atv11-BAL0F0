59e26b7dc54ec0cd705a7027de64e6e56f39a652
Pablo Novaes <pablo.novaes@estudante.ufjf.br>
2023-11-21 22:19:31 -0300

    cria 3 lis

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const olItens=docment.querySelector("#itens");
 const li=docment.createElement("li");
 li.textContent="Primeiro";
 olItens.appendChild(li);
+
+const li2=docment.createElement("li");
+li2.textContent="Segundo";
+olItens.appendChild(li2);
+
+const li3=docment.createElement("li");
+li3.textContent="Terceiro";
+olItens.appendChild(li3);
