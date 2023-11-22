59f7a8f3c2fff113afa8b97d5e48dc40bcbd5d63
Pablo Novaes <pablo.novaes@estudante.ufjf.br>
2023-11-21 22:15:03 -0300

    seleciona elementos

---
+++ main.js
@@ -0,0 +1,8 @@
+const pEntrada=docment.querySelector("#entrada");
+const btnAdicionar=docment.querySelector("#adicionar");
+const btnLimpar=docment.querySelector("#limpar");
+const olItens=docment.querySelector("#itens");
+
+const li=docment.createElement("li");
+li.textContent="Primeiro";
+olItens.appendChild(li);
